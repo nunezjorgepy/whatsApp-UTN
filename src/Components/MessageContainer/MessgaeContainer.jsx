9a6da0a144d9cb419ee700f4f870e6607cdada9c
Jorge Nuñez <nunez.jorge@hotmail.com>
2025-12-02 18:10:48 -0300

    Crea el input para escribir mensajes

--- src/Components/MessageContainer/MessgaeContainer.jsx
+++ src/Components/MessageContainer/MessgaeContainer.jsx
@@ -12,6 +12,14 @@ function MessgaeContainer() {
             <div className="msgs_container">
                 <Message />
             </div>
+
+            {/* Enviar Mensaje */}
+            <div className="send_message">
+                <input type="text" className="send_message_input" placeholder='Escribe un mensaje'/>
+                <button className="send_icon">
+                    <i class="bi bi-send"></i>
+                </button>
+            </div>
         </section>
     )
 }
