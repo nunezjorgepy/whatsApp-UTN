21e5d4c4605c3bd3d8db05266ed6054ca7312128
Jorge Nuñez <nunez.jorge@hotmail.com>
2025-12-02 17:35:41 -0300

    Crea el componente ChatHeader y cambia los elementos del archivo

---
+++ src/Components/ChatHeader/ChatHeader.jsx
@@ -0,0 +1,21 @@
+import './ChatHeader.css'
+
+function ChatHeader() {
+    return (
+        <div className="chat_header">
+            <button className="show_contact_info">
+                <div className="img_container">
+                    <img className='chat_img' src="https://photosmint.com/wp-content/uploads/2025/03/beautiful-whatsapp-dp-boy.jpg" alt="Foto de Contacto" />
+                </div>
+                <div className="chat_name">
+                    Marisa Delpodio
+                </div>
+            </button>
+            <a href="/" className="back_home">
+                <i class="bi bi-arrow-left"></i>
+            </a>
+        </div>
+    )
+}
+
+export default ChatHeader
--- src/Components/MessageContainer/MessgaeContainer.jsx
+++ src/Components/MessageContainer/MessgaeContainer.jsx
@@ -1,23 +1,12 @@
 import Message from '../Message/Message'
+import ChatHeader from '../ChatHeader/ChatHeader'
 import './MessgaeContainer.css'
 
 function MessgaeContainer() {
     return (
         <section className='msg_container'>
             {/* Info del contacto */}
-            <div className="chat_header">
-                <button className="show_contact_info">
-                    <div className="img_container">
-                        <img className='chat_img' src="https://photosmint.com/wp-content/uploads/2025/03/beautiful-whatsapp-dp-boy.jpg" alt="Foto de Contacto" />
-                    </div>
-                    <div className="chat_name">
-                        Marisa Delpodio
-                    </div>
-                </button>
-                <a href="/" className="back_home">
-                    <i class="bi bi-arrow-left"></i>
-                </a>
-            </div>
+            <ChatHeader />
 
             {/* Mensajes */}
             <div className="msgs_container">
