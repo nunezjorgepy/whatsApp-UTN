a2a939a762e55da0ee0c9c60161f1e7d4927f016
Jorge Nuñez <nunez.jorge@hotmail.com>
2025-12-07 04:48:36 -0300

    Setea mediante una función los mensajes mostrados en el chat y usa useEffect para llamar dicha función

--- src/Components/MessageContainer/MessgaeContainer.jsx
+++ src/Components/MessageContainer/MessgaeContainer.jsx
@@ -1,21 +1,40 @@
 import Message from '../Message/Message'
 import ChatHeader from '../ChatHeader/ChatHeader'
 import './MessgaeContainer.css'
-import { getContactList, getContactById } from "../../service/contactService"
+import { getContactById } from "../../service/contactService"
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 function MessgaeContainer() {
     const { id: id } = useParams()
+    const [HTMLMessages, setHTMLMessages] = useState([])
     const contact = getContactById(id)
 
-    const HTMLMessages = contact.messages.map(
+    /* const HTMLMessages = contact.messages.map(
         (message) => {
             return (
                 <Message key={message.message_id} message={message} />
             )
         }
+    ) */
+    function renderMessageComponent(){
+        /* Función para setear los Message Components que se mostrarán en el chat */
+        console.log('Here I am?')
+        const messages = contact.messages.map(
+            message => {
+                return(
+                    <Message key={message.message_id} message={message} />
+                )
+            }
+        )
+        
+        setHTMLMessages(messages)
+    }
+
+    useEffect(
+        renderMessageComponent,
+        [contact]
     )
-    
 
     return (
         <section className='msg_container'>
@@ -29,7 +48,11 @@ function MessgaeContainer() {
 
             {/* Enviar Mensaje */}
             <form className="send_message">
-                <input type="text" name='write_message_input' id='write_message_input' className="send_message_input" placeholder='Escribe un mensaje'/>
+                <input 
+                    type="text" name='write_message_input' 
+                    id='write_message_input' 
+                    className="send_message_input" 
+                    placeholder='Escribe un mensaje'/>
                 <button className="send_icon">
                     <i className="bi bi-send"></i>
                 </button>
