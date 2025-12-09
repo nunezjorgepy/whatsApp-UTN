0f1ab3bd69c948cdf0a90af2499e9c6efd09940e
Jorge Nuñez <nunez.jorge@hotmail.com>
2025-12-09 13:01:02 -0300

    Creado el formulario del nuevo contacto

--- src/Components/NewOption/NewOption.jsx
+++ src/Components/NewOption/NewOption.jsx
@@ -18,6 +18,30 @@ function NewOption() {
                     </button>
                 </div>
             </div>
+
+            <div className="new_contact_container">
+                <form className="new_contact_form">
+                    <div className="new_contact_name">
+                        <label htmlFor="new_name_input" className="new_contact_label new_name_label">
+                            <i class="bi bi-person"></i>
+                            Nombre Completo
+                        </label>
+                        <input type="text" className="new_contact_input new_name_input" id='new_name_input' placeholder='Ingresa el nombre completo' autoFocus/>
+                    </div>
+                    <div className="new_contact_phone">
+                        <label htmlFor="new_phone_input" className="new_contact_label new_phone_label">
+                            <i class="bi bi-telephone"></i>
+                            Teléfono
+                        </label>
+                        <input type="text" className="new_contact_input new_phone_input" id='new_phone_input' placeholder='1112345678'/>
+                    </div>
+
+                    <div className="new_btns_container">
+                        <button className="btn_new_form btn_add_new_contact">Agregar</button>
+                        <button className="btn_new_form btn_cancel_add">Cancelar</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
